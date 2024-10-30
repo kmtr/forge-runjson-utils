5efb6ca16ec6c07d243c467da98dfe2645c86faf
kmtr <kmtr@outlook.com>
2024-10-30 22:34:06 +0900

    update misc

--- src/RunJsonUtils.s.sol
+++ src/RunJsonUtils.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.25 <0.9.0;
+pragma solidity >=0.8.13 <0.9.0;
 
 import {VmSafe} from "forge-std/Script.sol";
 
--- test/RunJsonUtils.t.sol
+++ test/RunJsonUtils.t.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.25;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import "../src/RunJsonUtils.s.sol";
